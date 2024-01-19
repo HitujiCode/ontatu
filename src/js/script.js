fc1343d55df4720faa612e2a0a060d2da8c9903e
hituji_code <k.m.hituji3340@gmail.com>
2024-01-19 16:59:12 +0900

    利用規約大枠

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+  // ページトップボタン
+  $(window).on("scroll", function () {
+    let scrollPosition = $(this).scrollTop();
+    let toTop = $(".js-totop");
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    if (scrollPosition > 70) {
+      toTop.fadeIn();
+    } else {
+      toTop.fadeOut();
+    }
+  });
+  $(".js-totop").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
 
+  // タブ
+  $(".js-tab-head").on("click", function () {
+    $(".js-tab-head").removeClass("is-active");
+    $(".js-tab-content-item").removeClass("is-active");
+    $(this).addClass("is-active");
+    let number = $(this).data("number");
+    $("#" + number).addClass("is-active");
+  });
 
+  // アコーディオン
+  $(".js-faq-question").on("click", function () {
+    $(this).next().slideToggle();
+    $(this).toggleClass("is-open");
+    $("js-faq-answer").toggleClass("is-open");
+  });
 });
